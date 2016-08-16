281f16c5de392289d71525feb64113ce51a52db9
Manu Ignatius <manu@subnero.com>
2016-08-16 18:46:42 +0800

    Fixed Performative values from lower case to upper case

--- src/main/python/fjage/messages.py
+++ src/main/python/fjage/messages.py
@@ -23,20 +23,19 @@ class Action:
     SEND = "send"
     SHUTDOWN = "shutdown"
 
-# TODO: Verify whether to implement Performative as an enum (like in java)
 class Performative:
-    REQUEST = "request"                        # Request an action to be performed.
-    AGREE = "agree"                            # Agree to performing the requested action.
-    REFUSE = "refuse"                          # Refuse to perform the requested action.
-    FAILURE = "failure"                        # Notification of failure to perform a requested or agreed action.
-    INFORM = "inform"                          # Notification of an event.
-    CONFIRM = "confirm"                        # Confirm that the answer to a query is true.
-    DISCONFIRM = "disconfirm"                  # Confirm that the answer to a query is false.
-    QUERY_IF = "query_if"                      # Query if some statement is true or false.
-    NOT_UNDERSTOOD = "not_understood"          # Notification that a message was not understood.
-    CFP = "cfp"                                # Call for proposal.
-    PROPOSE = "prpopose"                       # Response for CFP.
-    CANCEL = "cancel"                          # Cancel pending request.
+    REQUEST = "REQUEST"                         # Request an action to be performed.
+    AGREE = "AGREE"                             # Agree to performing the requested action.
+    REFUSE = "REFUSE"                           # Refuse to perform the requested action.
+    FAILURE = "FAILURE"                         # Notification of failure to perform a requested or agreed action.
+    INFORM = "INFORM"                           # Notification of an event.
+    CONFIRM = "CONFIRM"                         # Confirm that the answer to a query is true.
+    DISCONFIRM = "DISCONFIRM"                   # Confirm that the answer to a query is false.
+    QUERY_IF = "QUERY_IF"                       # Query if some statement is true or false.
+    NOT_UNDERSTOOD = "NOT_UNDERSTOOD"           # Notification that a message was not understood.
+    CFP = "CFP"                                 # Call for proposal.
+    PROPOSE = "PROPOSE"                         # Response for CFP.
+    CANCEL = "CANCEL"                           # Cancel pending request.
 
 class Message:
     """Base class for messages transmitted by one agent to another.
